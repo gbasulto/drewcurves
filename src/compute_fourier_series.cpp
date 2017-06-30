98035ca4c46b9b549532b494c898dddee803fad2
Guillermo Basulto <guillermobasulto@gmail.com>
2017-06-30 16:35:23 -0500

    Add function that computes Fourier series.

---
+++ src/compute_fourier_series.cpp
@@ -0,0 +1,36 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//--------------------------------------------------------------------
+  
+// [[Rcpp::export]]
+NumericMatrix compute_fourier_series(NumericMatrix mat,
+                                     NumericVector t, int type)
+{
+  int l, k, n, K = t.size(), N = mat.nrow(), M = mat.ncol(),
+  L = (M - 1)/2;
+  double arg;
+  NumericMatrix out(K, N);
+  
+  for (k = 0; k < K; k++)
+  {
+    for (n = 0; n < N; n++)
+    {
+      l = 0;
+      out(k, n) = mat(n, 0)/pow(2.0, 0.5);
+      while (l < L)
+      {
+        l++;
+        arg = l*t[k];
+        out(k, n) +=
+          mat(n, 2*l - 1)*sin(arg) + mat(n, 2*l)*cos(arg);
+      }
+      if (M % 2 == 0)
+      {
+        out(k, n) += mat(n, M - 1)*sin(l*M/2);
+      }
+    }
+  }
+  return out;
+}
+
