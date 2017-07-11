f42408f5738299b2191cc788986af33fdbb31160
Guillermo Basulto <guillermobasulto@gmail.com>
2017-07-11 16:38:35 -0500

    Add line to possibly fix the Windows warning.

--- R/zzz.R
+++ R/zzz.R
@@ -6,3 +6,6 @@ NULL
 .onUnload <- function (libpath) {
   library.dynam.unload("drewcurves", libpath)
 }
+
+# ## Uncomment the next line and run it every time a C++ function is added
+# tools::package_native_routine_registration_skeleton(".", "src/init.c")
