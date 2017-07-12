f2a82cabc99b4f5517dd79432d5bb18c279e5659
Guillermo Basulto <guillermobasulto@gmail.com>
2017-07-12 12:14:32 -0500

    Add minmax normalization.

---
+++ R/minmax_normalization.R
@@ -0,0 +1,27 @@
+
+##' Min-max Normalization
+##'
+##' Normalize columns by substracting its minimum and dividing by the
+##' length of its range. The resulting columns will be between zero
+##' and one.
+##' @param X Vector, matriz or dataframe. 
+##' @return An object of the same type and dimensions as X.
+##' @examples
+##' data(iris)
+##' minmax_normalization(iris[, -5])
+##' @author Guillermo Basulto-Elias
+##' @export
+minmax_normalization <- function (X) {
+
+    ## Normalize vector
+    if (is.vector(X)) {
+        min_X <- min(X)
+        range <- max(X) - min_X
+        return ((X - min_X)/range)
+    }
+    
+    ## Normalize matrix or dataframe by column
+    apply(X, 2, minmax_normalization)
+}
+
+
